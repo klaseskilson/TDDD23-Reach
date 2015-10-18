a1ef4af5be14f1e399a854768271f26dad7701f7
Klas Eskilson <klas.eskilson@gmail.com>
2015-10-18 21:18:16 +0200

    Initial work on level2

--- game/main.js
+++ game/main.js
@@ -11,6 +11,7 @@ Reach.prototype = {
     game.state.add('menu', MenuState);
     game.state.add('intro', IntroState);
     game.state.add('level1', Level1State);
+    game.state.add('level2', Level2State);
 
     game.state.start('boot');
   }
--- game/states/preload.js
+++ game/states/preload.js
@@ -12,6 +12,7 @@ PreloadState.prototype = {
 
     self.game.load.tilemap('intro', 'assets/tilemaps/intro.json', null, Phaser.Tilemap.TILED_JSON);
     self.game.load.tilemap('level1', 'assets/tilemaps/level1.json', null, Phaser.Tilemap.TILED_JSON);
+    self.game.load.tilemap('level2', 'assets/tilemaps/level2.json', null, Phaser.Tilemap.TILED_JSON);
     self.game.load.image('reachSprites', 'assets/images/reach_sprite.png');
     self.game.load.image('background', 'assets/images/dawn.png');
     self.game.load.image('menuBackground', 'assets/images/menu.png');
